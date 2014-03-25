99f7b9eabee618ecdce601a8e814050247074d3a
Tony Tam <fehguy@gmail.com>
2014-03-25 10:18:32 -0700

    updated script

--- modules/swagger-utils/bin/extractModels.sh
+++ modules/swagger-utils/bin/extractModels.sh
@@ -1,3 +1,19 @@
-scala -cp $(echo \'target/*:target/lib/*:$2\') ModelExporter $1 out.txt
+#!/usr/bin/env bash
+echo "" > classpath.txt
+for file in `ls target/lib`;
+        do echo -n 'target/lib/' >> classpath.txt;
+        echo -n $file >> classpath.txt;
+        echo -n ':' >> classpath.txt;
+done
+for file in `ls target`;
+        do echo -n 'target/' >> classpath.txt;
+        echo -n $file >> classpath.txt;
+        echo -n ':' >> classpath.txt;
+done
+echo -n ':' >> classpath.txt;
+echo -n $2 >> classpath.txt;
+export CLASSPATH=$(cat classpath.txt)
+
+scala -cp $CLASSPATH ModelExporter $1 out.txt
 
 node ./src/main/js/extractModels.js $@
