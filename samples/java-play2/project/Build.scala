9146edd10829d622ca12bb759060718b66e13234
Tony Tam <fehguy@gmail.com>
2014-03-29 13:15:13 -0700

    updated version

--- samples/java-play2/project/Build.scala
+++ samples/java-play2/project/Build.scala
@@ -5,11 +5,11 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName = "petstore"
-  val appVersion = "1.3.3-SNAPSHOT"
+  val appVersion = "1.3.4"
 
   val appDependencies: Seq[sbt.ModuleID] = Seq(
-    "com.wordnik" %% "swagger-play2" % "1.3.3-SNAPSHOT",
-    "com.wordnik" %% "swagger-play2-utils" % "1.3.3-SNAPSHOT"
+    "com.wordnik" %% "swagger-play2" % "1.3.4",
+    "com.wordnik" %% "swagger-play2-utils" % "1.3.4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
